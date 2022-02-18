b92434bb51025b3ce8dd5e2d37a53d7290e6d420
Mateusz Slomka <slomkam7@gmail.com>
2022-02-18 10:18:58 +0100

    change text

--- index.js
+++ index.js
@@ -52,5 +52,5 @@ app.listen( process.env.PORT || 3000, () => {
   console.log('App is listening on port ' + process.env.PORT || 3000)
 });
 app.get('/', (req, res) =>{
-  res.send('App is working');
+  res.send('Bot is working');
 })
