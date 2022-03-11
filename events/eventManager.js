9dfd6497dfdc0c9ace9924ca79e995ea3235423e
Mateusz Slomka <slomkam7@gmail.com>
2022-03-11 17:27:10 +0100

    remove console log

--- events/eventManager.js
+++ events/eventManager.js
@@ -83,7 +83,7 @@ export class EventManager {
   #isTime( event ) {
     const d = new Date();
     const now = new Date( d.getTime() + ( 1 /** h */ * 60 * 60 * 1000 ) );
-    console.log( now );
+    
     return (event.day != undefined ? now.getDate() == event.day : true) &&
       (event.hour != undefined ? now.getHours() == event.hour : true) &&
       (event.minute != undefined
